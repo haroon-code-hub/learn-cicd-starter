9f1a9300a0ae41b042f7fd89b65d9f20c06c11f1
Haroon Saeed <haroon.saeed@outlook.com>
2026-02-15 20:31:53 +0100

    Fix security issues

--- json.go
+++ json.go
@@ -30,5 +30,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat) // #nosec G705 -- JSON payload is an intentional API response body
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
+	}
 }
